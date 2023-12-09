e1a052061e8c21379bb97810997b4fb6a65ec33b
Josef Strillinger <jstrillinger@outlook.com>
2023-12-09 23:31:56 +0100

    day 2: part 2 completed

---
+++ day-02/src/bin/part2.rs
@@ -0,0 +1,67 @@
+#[derive(Debug)]
+pub struct Round {
+    blue: i32,
+    red: i32,
+    green: i32,
+} 
+
+fn main() {
+    let input = include_str!("./input2.txt");
+
+    let data = parse(input);
+    //dbg!(data);
+
+    let output = process(&data);
+    println!("Output: {}", output);
+
+}
+
+pub fn parse(input: &str) -> Vec<Round> { // Final Outpout will be Result with Error and A Vect of Rounds
+    let mut v: Vec<Round> = Vec::new();
+    for line in input.lines(){
+        let mut round = Round{
+            blue: 0,
+            red: 0,
+            green: 0,
+        };
+        let data = line.split(":").collect::<Vec<&str>>()[1].trim();
+        // replace ; with , in data
+        let data = data.replace(";", ",");
+
+        // split data by ,
+        let data = data.split(",").collect::<Vec<&str>>();
+
+        for entry in data {
+            let entry_data = entry.trim().split(" ").collect::<Vec<&str>>();
+            let int_entry_data = entry_data[0].parse::<i32>().unwrap();
+            match entry_data[1]{
+                "blue" => {
+                    if int_entry_data > round.blue {
+                        round.blue = int_entry_data;
+                    }
+                },
+                "red" => {
+                    if int_entry_data > round.red {
+                        round.red = int_entry_data;
+                    }
+                },
+                "green" => {
+                    if int_entry_data > round.green {
+                        round.green = int_entry_data;
+                    }
+                },
+                _ => {},
+            }
+        }
+        v.push(round);
+    }
+    v
+}
+
+pub fn process(input: &Vec<Round>) -> i32{
+    let mut output = 0; 
+    for entry in input {
+        output = output + (entry.blue * entry.red * entry.green);
+    }
+    output
+} 
